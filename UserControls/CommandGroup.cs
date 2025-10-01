cf9e949233e3311487f295a35348e6dae3377bbf
ornaras <ornaras.us@gmail.com>
2025-10-01 09:54:00 +0400

    Ручная доработка дизайнера Visual

--- UserControls/CommandGroup.cs
+++ UserControls/CommandGroup.cs
@@ -138,5 +138,46 @@ namespace PCBS.UserControls
             MainForm.CurrentConnection?.Get(cmdReset.Value);
             Pull(sender, e);
         }
+
+        #region UI
+        protected static Label CreateLabel(string text, int y, int w) => new Label()
+        {
+            Location = new Point(3, y),
+            Size = new Size(w, 13),
+            Text = text
+        };
+
+        protected static CheckBox CreateCheckBox(string text, string tag, int y, int w) => new CheckBox()
+        {
+            Location = new Point(3, y),
+            Size = new Size(w, 17),
+            Tag = tag,
+            Text = text,
+        };
+
+        protected static NumericUpDown CreateNumberBox(string tag, int x, int y, int w)
+        {
+            var control = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(control)).BeginInit();
+            control.Location = new Point(x, y);
+            control.Maximum = 99999;
+            control.Size = new Size(w, 20);
+            control.Tag = tag;
+            ((System.ComponentModel.ISupportInitialize)(control)).EndInit();
+            return control;
+        }
+
+        protected static ComboBox CreateComboBox(string tag, int x, int y, int w, params object[] items)
+        {
+            var control = new ComboBox()
+            {
+                Location = new Point(x, y),
+                Size = new Size(w, 21),
+                Tag = tag
+            };
+            control.Items.AddRange(items);
+            return control;
+        }
+        #endregion
     }
 }
